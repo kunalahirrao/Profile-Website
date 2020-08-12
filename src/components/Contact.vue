f39fb3ba068038c9a2b6c99f86678301f98b0cef
unknown <erahirraokunal@gmail.com>
2020-08-12 18:10:06 +0530

    Same as above

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -80,7 +80,7 @@ export default {
         .then(() => {
           this.diContactForm()
         })
-        .catch((e) => {
+        .catch(() => {
          alert("Something went wrong!!!!!!!!!!!!!!");
         });
     },
